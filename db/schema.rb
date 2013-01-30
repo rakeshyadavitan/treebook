8d1a2bb5ae39f57b899b8c42689173b92baac77b
rakesh <rakeshyadavitan@yahoo.com>
2013-01-30 19:33:12 +0530

    updated front end for new authentication
    added simple_form
    updated several form to bootstrap/simple_form
    added login/logout/register/progile to nav
    added collection select to our status form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130129105755) do
+ActiveRecord::Schema.define(:version => 20130129120450) do
 
   create_table "statuses", :force => true do |t|
     t.text     "content"
